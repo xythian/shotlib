6a8a0a7375f17138f00d154119ba43d1d80225a0
fox <fox@7e82af7f-0629-0410-be36-c8f2cdeb922f>
2008-08-30 23:01:52 +0000

    Re-add shotlib.permissions
    git-svn-id: svn+ssh://vault.xythian.com/shotlib/trunk@314 7e82af7f-0629-0410-be36-c8f2cdeb922f

---
+++ shotlib/permissions.py
@@ -0,0 +1,56 @@
+from shotlib.context import context
+from shotlib.enums import create_enum
+from shotlib.util import copyinfo
+
+class PermissionDenied(Exception):
+    pass
+
+class Permission(object):
+    def __init__(self):
+        self.name = 'unknown'
+
+    def _create_perm(self):
+        def execute(target, user=None):
+            return self.test(target, user=user)
+        execute.require = self.require
+        execute.verify = self.verify
+        return execute
+
+    def __call__(self, func):
+        self.__base = func
+        self.name = func.__name__     
+        return self._create_perm()
+
+    @property
+    def context(self):
+        return context()
+
+    def test(self, target, user=None):
+        assert target is not None
+        if user is None:
+            user = self.context.user
+        try:
+            tgt = getattr(target, '_permission_' + self.name)
+        except AttributeError:
+            return self.__base(target, user)
+        return tgt(user)
+    
+    def require(self, target):
+        if target is None:
+            raise PermissionDenied("%s cannot %s %s" % (self.context.user, self.name, target))
+        if not self.test(target):
+            raise PermissionDenied("%s cannot %s %s" % (self.context.user, self.name, target))
+        return target
+
+    def verify(self, func):
+        @copyinfo(func)
+        def _verify(*args, **kwargs):
+            return self.require(func(*args, **kwargs))
+        return _verify
+
+    def requires(self, func):
+        @copyinfo(func)
+        def wrapped(*args, **kwargs):
+            self.require(args[0])
+            return func(*args, **kwargs)
+        return wrapped
