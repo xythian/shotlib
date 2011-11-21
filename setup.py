39da9647d53fddf0dca568b3251cb059d1b603d4
Ken Fox <fox@xythian.com>
2011-11-20 16:21:53 -0800

    Bump version to 0.5; add .gitignore.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup (
     name = "shotlib",
-    version = "0.3",
+    version = "0.5",
     packages = find_packages(),
     author = "Ken Fox",
     author_email = "fox@xythian.com",
